73baf6ece314243521ff1424f338267da139080f
allegro <allegrocanttabile@gmail.com>
2020-07-18 09:20:07 -0500

    subiendo al git

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel crud</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+    
+</body>
+</html>
